6ae915e819538e97c8979cc4b972c5065ed1f634
ge1s7 <kacperkalinowski@protonmail.com>
2024-11-18 04:08:23 +0100

    wrapper works as intended

--- main.py
+++ main.py
@@ -4,44 +4,35 @@ from curses import wrapper
 import time
 import random
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(True)
-curses.curs_set(1)
+def main(stdscr):
+    siatka = Grid(299,60)
+    siatka.init_grid()
+    siatka.randomize()
 
-siatka = Grid(172,40)
-siatka.init_grid()
-siatka.randomize()
+    #win = curses.newwin(siatka.height, siatka.width, 7, 20)
 
-#win = curses.newwin(siatka.height, siatka.width, 7, 20)
-
-generation = 1
+    generation = 1
 
 
-while True:
-    
+    while True:       
 
-    x,y = 0,0   
-    while y < siatka.height:
-        while x < siatka.width:
-            stdscr.addstr(y,x,str(siatka.grid_list[(x,y)]))
-            x += 1
-        if x >= siatka.width:
-            x = 0
-            y += 1
+        x,y = 0,0   
+        while y < siatka.height:
+            while x < siatka.width:
+                stdscr.addstr(y,x,str(siatka.grid_list[(x,y)]))
+                x += 1
+            if x >= siatka.width:
+                x = 0
+                y += 1
 
 
 
-    siatka.c_eval()
-    generation += 1
-    stdscr.addstr(siatka.height+1, 0, f"gen: {generation}")
-    stdscr.addstr(siatka.height+2, 0, f"cursor_pos: {curses.getsyx()}")
-    time.sleep(0.01)
-    
-    stdscr.refresh()
+        siatka.c_eval()
+        generation += 1
+        stdscr.addstr(siatka.height+1, 0, f"gen: {generation}")
+        stdscr.addstr(siatka.height+2, 0, f"cursor_pos: {curses.getsyx()}")
+        time.sleep(0.00000000001)
+        
+        stdscr.refresh()
 
-curses.nocbreak()
-stdscr.keypad(False)
-curses.echo()
-curses.endwin()
+wrapper(main)
