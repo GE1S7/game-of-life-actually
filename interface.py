e3f57a34b8a07cb971d476d1ed34b0ecc2b1bede
ge1s7 <kacperkalinowski@protonmail.com>
2025-04-21 09:12:21 +0200

    FIX cursor navigation logic

--- interface.py
+++ interface.py
@@ -10,15 +10,23 @@ def invert_boolean(p):
 
 
 def interface(stdscr, factory=None):
+    DEAD = "░"
+    ALIVE = "█"
+
     grid = factory.grid
     stdscr.clear()
 
     curses.curs_set(1) # set cursor visibility
+    curses.start_color()
     curses.use_default_colors()
     curses.can_change_color()
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
     stdscr.nodelay(True) # don't stall the screen while waiting for the input
     keypress = None
+    
     pad = curses.newpad(grid.height, grid.width)
+    padposx,padposy = (0,0)
     cursor_y,cursor_x = pad.getyx()
     py,px = 0,0
 
@@ -33,16 +41,21 @@ def interface(stdscr, factory=None):
         x,y = 0,0   
         while y < grid.height:
             while x < grid.width:
-                #stdscr.addstr(y,x,str(grid.grid_list[(x,y)]))
                 try:
-                    pad.addstr(y,x,str(grid.grid_list[(x,y)]))
+                    if (x,y) in grid.selected:
+                        pad.addstr(y,x,str(grid.grid_list[(x,y)]), curses.color_pair(47))
+                    else:
+                        pad.addstr(y,x,str(grid.grid_list[(x,y)]))
                 except curses.error:
                     pass
                 x += 1
             if x >= grid.width:
                 x = 0
                 y += 1
-        
+
+        for cell in grid.selected:
+            pad.addch(cell[1], cell[0], str(grid.grid_list[cursor_x,cursor_y]), curses.A_DIM)
+            pad.addch(cell[1], cell[0], str(grid.grid_list[cursor_x,cursor_y]), curses.color_pair(47))
         cy, cx = curses.getsyx()
         # For testing purposes:
         # display cursor location
@@ -56,8 +69,9 @@ def interface(stdscr, factory=None):
         #pad.addstr(py+cy+2,cx+10, f"pad_size: {str(pad.getmaxyx())}")
 
         # show id of key pressed
-        pad.addstr(py+cy+3, cx+1, f"key pressed: {keypress}")
-        pad.addstr(py+cy+4,cx+1, f"selected: {grid.selected}")
+        #pad.addstr(py+cy+3, cx+1, f"key pressed: {keypress}")
+        #pad.addstr(py+cy+4,cx+1, f"selected: {grid.selected}")
+        #pad.border()
 
 
 
@@ -67,19 +81,19 @@ def interface(stdscr, factory=None):
         # cursor movement
         pad.move(cursor_y, cursor_x)
         keypress = stdscr.getch()
-        if keypress == curses.KEY_UP or keypress == 107 and cursor_y-1 >= 0:
+        if (keypress == curses.KEY_UP or keypress == 107) and cursor_y-1 >= 0:
             cursor_y -= 1
             if 5 >=  cy:
                 py -= 1
-        elif keypress == curses.KEY_DOWN or keypress == 106 and cursor_y < grid.height-1:
+        elif (keypress == curses.KEY_DOWN or keypress == 106) and cursor_y < grid.height-1:
             cursor_y += 1
             if window_size_y - 5 <=  cy:
                 py += 1
-        elif keypress == curses.KEY_RIGHT or keypress == 108 and cursor_x < grid.width-1:
+        elif (keypress == curses.KEY_RIGHT or keypress == 108) and cursor_x < grid.width-1:
             cursor_x += 1
             if window_size_x - 5 <=  cx:
                 px += 1
-        elif keypress == curses.KEY_LEFT or keypress == 104 and cursor_x-1 >= 0: 
+        elif (keypress == curses.KEY_LEFT or keypress == 104) and cursor_x-1 >= 0: 
             cursor_x -= 1
             if 5 >=  cx:
                 px -= 1
@@ -109,15 +123,9 @@ def interface(stdscr, factory=None):
             return 
 
         #print(f"\n\nnwindow size: {stdscr.getmaxyx()}")
-
-
-            
-            
-
-        
         #print("py, px: ", py, px)
         #print("py: ", py, "window size y: ", window_size_y, "cursor_y ", cursor_y)
         
         stdscr.refresh()
-        pad.refresh(py, px, 0, 0, window_size_y-1, window_size_x-1) 
+        pad.refresh(py, px, padposy, padposx, window_size_y-1, window_size_x-1) 
         time.sleep(0.01666)
