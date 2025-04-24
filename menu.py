35e379081e4c8ac899d1bd44aea89ae2ab6f2f99
ge1s7 <kacperkalinowski@protonmail.com>
2025-04-24 20:35:17 +0200

    a WIP: FIX stdscr flickering

---
+++ menu.py
@@ -0,0 +1,10 @@
+class Menu():
+    def __init__(self, width, height, posx, posy, options):
+        self.width = width
+        self.height = height
+        self.posx = posx
+        self.posy = posy
+        self.posx_bot = posx
+        self.options = {"Singleplayer":, "Multiplayer":{"Co-op":, "Versus"":}}
+
+
